9dbac4a8821ee6fa0cc0b5c7527f4c6a034c2569
dasafe <48550353+dasafe@users.noreply.github.com>
2019-09-27 13:12:55 +0200

    Update Ej4.java

--- src/practica/Ej4.java
+++ src/practica/Ej4.java
@@ -4,7 +4,24 @@ public class Ej4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner (System.in);
+		String ruta = reader.nextLine();
+		File f = new File("ruta");
+		
+		//Comprobamos la ruta
+		if(f.isFile()){
+			//Imprime contenido del txt
+			try (BufferedReader br = new BufferedReader(new FileReader("foo.txt"))) {
+				String line = null;
+				while ((line = br.readLine()) != null) {
+		 			System.out.println(line);
+				}
+			}
+		}else if(f.isDirectory()){
+			System.out.println("La ruta es un directorio");
+		}else{
+			System.out.println("La ruta no existe");
+		}
 	}
 
 }
